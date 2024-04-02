efcf7923392782b3584271a907773ac86f143cbf
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2024-04-02 12:26:11 -0300

    CORS autenticacao

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.post('/',(req,res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Accept-  Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+
     const {convert} = req.body
     const {string} = req.body
 
